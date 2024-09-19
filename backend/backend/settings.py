adfd3d6174175c3bf9d98f7596e26bea0eee7936
vihaan-sood <vihaan.sood0@gmail.com>
2024-09-19 17:03:01 +0100

    debugging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "api",
+    "../api",
     "rest_framework",
     "corsheaders",
 
