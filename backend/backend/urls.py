d71ce7b724ae4f7e7bbff260a6f250d723a5dd26
vihaan-sood <vihaan.sood0@gmail.com>
2024-09-19 19:08:02 +0100

    url addition for error

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,14 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.http import HttpResponse
 
 from api.views import *
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-
+def home(request):
+    return HttpResponse("Welcome to the Active Materials Library Backend!")
 
 
 urlpatterns = [
