dd68209a3467b67c049108f153f06274cbfef397
vihaan-sood <vihaan.sood0@gmail.com>
2024-09-19 17:58:33 +0100

    debugging_3

--- backend/api/wsgi.py
+++ backend/api/wsgi.py
@@ -1,6 +1,8 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/render/project/src/backend')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","backend.backend.settings")
 
