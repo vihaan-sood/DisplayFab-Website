47010f9d549389ba81806035885f9ada32ef403b
vihaan-sood <vihaan.sood0@gmail.com>
2024-09-22 19:23:34 +0100

    added frontend url to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,12 +86,16 @@ INSTALLED_APPS = [
 
 
 
-#CHANGE LATER FOR SECURITY###########################
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get("FRONTEND_URL"),  #frontend url
+]
+
+
 CORS_ALLOWS_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://library.co.uk/",  # Frontend origin
+    os.environ.get("FRONTEND_URL"),  # Frontend origin
 ]
 
 MIDDLEWARE = [
