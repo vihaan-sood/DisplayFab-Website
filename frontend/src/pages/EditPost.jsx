c96fe15722095116e4b12f76e033a2014da1bed8
vihaan-sood <vihaan.sood0@gmail.com>
2024-09-23 22:28:40 +0100

    Revert "post edit 1"
    This reverts commit d4f38af3d87cc54f636eff9c63117cde9cd63040.

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -1,37 +1,12 @@
-import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PostEditForm from "../components/PostEditForm";
 import Header from "../components/Header";
-import api from "../api"; 
 
 const EditPostPage = () => {
-    const [user, setUser] = useState('');
-    const [error, setError] = useState(null); 
-
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await api.get('/api/user/currentuser/', { requiresAuth: true });
-                setUser(response.data || ''); 
-            } catch (err) {
-                console.error("Error fetching user data:", err);
-                setError('Failed to load current user data.');
-            }
-        };
-
-        fetchUser(); 
-    }, []); 
-
     return (
         <>
             <Header />
-            {error ? (
-                <p>{error}</p>
-            ) : (
-                <>
-                    <PostEditForm />
-                </>
-            )}
+            <PostEditForm />
         </>
     );
 };
